0d966bd6f67c017586c8f9ab39d00f2941c80743
Chirag Rupani <36813124+ChiragRupani@users.noreply.github.com>
2025-12-28 22:03:26 +0530

    Updated oxlint and improved tests

--- tests/TestUtils.ts
+++ tests/TestUtils.ts
@@ -2,10 +2,10 @@ import assert from "node:assert/strict";
 
 export function assertContains(vals: string[], searchElement: string) {
   let hasValue = vals.includes(searchElement);
-  assert.ok(hasValue, `Expected [${vals}] to include '${searchElement}'`);
+  assert.ok(hasValue, `Expected [${vals.join()}] to include '${searchElement}'`);
 }
 
 export function assertNotContains(vals: string[], searchElement: string) {
   let hasValue = vals.includes(searchElement);
-  assert.ok(!hasValue, `Expected [${vals}] not to include '${searchElement}'`);
+  assert.ok(!hasValue, `Expected [${vals.join()}] not to include '${searchElement}'`);
 }
